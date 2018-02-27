ff7015208e5731fe79ddfb770c0e9ff718912bc5
Rotl <940043033@qq.com>
2018-02-27 19:59:57 +0800

    集成element-ui完成

---
+++ src/component/admin/Admin.vue
@@ -0,0 +1,66 @@
+<template>
+    <el-container>
+
+        <!-- 左侧 -->
+        <el-aside width="200px">
+            <!-- 左侧导航, 公共部分 -->
+            <app-aside></app-aside>
+        </el-aside>
+
+        <!-- 右侧 -->
+        <el-container>
+            <!-- 右侧头部, 公共部分 -->
+            <el-header>Header</el-header>
+
+            <!-- 右侧主体, 变化部分, 将来要通过子路由控制 -->
+            <el-main>Main</el-main>
+        </el-container>
+
+    </el-container>
+</template>
+
+<script>
+// 导入公共的左侧与头部组件, 需要注册才能使用
+import AsideComponent from "./frame/Aside.vue";
+import HeaderComponent from "./frame/Header.vue";
+
+export default {
+  // 注册子组件, key为组件名称(将=使用时的标签名称), value为未注册的组件
+  components: {
+    "app-aside": AsideComponent,
+    "app-header": HeaderComponent
+  }
+};
+</script>
+
+<style scoped lang="less">
+// 页面架构高度固定, 左侧与右侧高度独自控制
+.el-container {
+  height: 100%;
+
+  // 左侧导航
+  .el-aside {
+    height: 100%;
+    background-color: #d3dce6;
+    color: #333;
+  }
+
+  // 右侧
+  .el-container {
+    height: 100%;
+
+    // 右侧头部
+    .el-header {
+      background-color: #b3c0d1;
+      color: #333;
+      line-height: 60px;
+    }
+
+    // 右侧主体
+    .el-main {
+      background-color: #e9eef3;
+      color: #333;
+    }
+  }
+}
+</style>
